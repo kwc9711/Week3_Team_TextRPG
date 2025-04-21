6cf8481f84e2c87aa1ef70d66af07cac8cdde441
JeonCM <wsd3576@gmail.com>
2025-04-21 16:16:39 +0900

    적 스크립트 추가
    -Enemy.cs 추가

---
+++ TextRPG/TextRPG_Week3/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Week3
+{
+    public class Enemy
+    {
+        public string Name;
+        public int Attack;
+        public int Hp;
+
+        public List<Enemy> enemyList = new List<Enemy>
+                {
+                });
+    }
+}
--- TextRPG/TextRPG_Week3/obj/Debug/net8.0/TextRPG_Week3.AssemblyInfo.cs
+++ TextRPG/TextRPG_Week3/obj/Debug/net8.0/TextRPG_Week3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG_Week3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3feceb5289447b96be209894f7c218dfde5989d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c9eebf368289682f67ea4116c4fb31c7ea8106")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG_Week3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextRPG_Week3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
